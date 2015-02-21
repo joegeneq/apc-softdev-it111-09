447edc09dee2650b6e8ceec7c9f523941b94d164
Jhun Michael Tabudlong <tabudlongj@gmail.com>
2015-02-21 23:39:45 +0800

    Dropdownlist on Province

--- practice/jotabudlong/basic/views/Province/_form.php
+++ practice/jotabudlong/basic/views/Province/_form.php
@@ -1,7 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\Region;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Province */
@@ -13,11 +15,14 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'province_code')->textInput(['maxlength' => 32]) ?>
-
+	
     <?= $form->field($model, 'province_description')->textInput(['maxlength' => 32]) ?>
 
     <?= $form->field($model, 'region_id')->textInput() ?>
-
+	<?= $form->field($model, 'region_id')->dropDownList(
+			ArrayHelper::map(Region::find()->all(),'region_id','region_description'),
+		['prompt'=>'Select Region']
+	)?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
