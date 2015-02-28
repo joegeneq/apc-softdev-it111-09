b7efa84443b24d13db9fad6ea2a43e162863dc47
Jhun Michael Tabudlong <tabudlongj@gmail.com>
2015-02-28 11:01:03 -0800

    Changing the database connection of ourspace to connect to my softdev_tabudlong_jhunmichael_ourspace

--- practice/midterm_test/Tabudlong_Jhun_Michael_midterms/ourspace/config/db.php
+++ practice/midterm_test/Tabudlong_Jhun_Michael_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_tabudlong_jhunmichael_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
