d55180e684ae2547f52d4fdfab1dc8e29ca47ba2
Martin Ayson <iammartinayson@gmail.com>
2015-03-13 19:42:44 +0800

    Modified frontend/views/layouts/main.php

--- application/fapsys/frontend/views/layouts/main.php
+++ application/fapsys/frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'Fullbright Assistance Program',
+                'brandLabel' => 'Welcome to Fulbright Assistance Program',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
@@ -34,8 +34,8 @@ AppAsset::register($this);
             ]);
             $menuItems = [
                 ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
+                ['label' => 'Grading System', 'url' => ['/site/about']],
+                ['label' => 'Process and Information', 'url' => ['/site/contact']],
             ];
             if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
@@ -65,7 +65,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; Fulbright Science High <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Fulbright Science Highschool <?= date('Y') ?></p>
       
         </div>
     </footer>
