e07e949d4a58629ebafc53379d47bb44cf8261a6
Martin Ayson <iammartinayson@gmail.com>
2015-04-01 00:57:33 +0800

    Modified <p> and <h2> headings.

--- application/fulbright_aps/frontend/views/site/index.php
+++ application/fulbright_aps/frontend/views/site/index.php
@@ -1,15 +1,16 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'Fulbright Science School System';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+       <p id="WelcomeHeading">Welcome!<p>
+        <h2>Fulbright Science High School Assistance Program</h2>
+        <p id="Register">New here? Please Register Below</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com"></a></p>
     </div>
 
     <div class="body-content">
