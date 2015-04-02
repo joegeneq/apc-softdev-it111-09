2fd19cd27ce30c85688ffd270020c1a4b582b7d2
Martin Ayson <iammartinayson@gmail.com>
2015-04-02 17:10:55 +0800

    Modifed index.php for the main page.

--- application/fulbright_aps/frontend/views/site/index.php
+++ application/fulbright_aps/frontend/views/site/index.php
@@ -19,22 +19,23 @@ $this->title = 'Fulbright Science School System';
             <div class="col-lg-4">
                 <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <p id="WhyEnroll">Why enroll at Fulbright Science School?</p>
+				<p id="Mission">Mission </p>
+                <p>	Fulbright Science School is committed to giving quality education and values
+                	formation. It shall become and ideal learning institution for 
+                	young boys and girls in making them active learners through 
+                	exploration and discovery. The students are encouraged to use 
+                	their reasoning skills, practice listening, explore their 
+                	environment and express, understand and handle their feelings 
+                	intellectually. Each school activity shall offer the students the 
+                	opportunity to establish confidence, responsibility and independence. </p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href=""> &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
