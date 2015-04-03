52ca0d95773fe88427b658db728f0cb42fa43cd5
Martin Ayson <iammartinayson@gmail.com>
2015-04-03 14:36:26 +0800

    Modified School Requirements on index.php

--- application/fulbright_aps/frontend/views/site/index.php
+++ application/fulbright_aps/frontend/views/site/index.php
@@ -29,7 +29,7 @@ $this->title = 'Fulbright Science School System';
 
 
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href=""> &raquo;</a></p>
             </div>
 
 <!--Edit for Body(Mission) Part-->            
@@ -49,14 +49,11 @@ $this->title = 'Fulbright Science School System';
             </div>
             <div class="col-lg-4">
 <!--Edit for Body(Part 3) Part-->
-                <h2>Heading</h2>
+                <p id="WhatItTakes">Knowing the requirements</p>
+                <p id="Information"> Transferee? Incoming freshmen? Or returning student?
+				<p id="SchoolRequirements">We're now accepting new applicants. Click the button to see the requirements. </p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="fontend\views\site\requirements.php>">School Requirements&raquo;</a></p>
             </div>
         </div>
 
