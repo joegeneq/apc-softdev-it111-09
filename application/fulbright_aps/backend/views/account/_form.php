b6f65bc4944e5e89a5c771916bc64571221b8f76
Jhun Michael Tabudlong <tabudlongj@gmail.com>
2015-04-03 16:15:25 -0700

    Account

---
+++ application/fulbright_aps/backend/views/account/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Account */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="account-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'account_username')->textInput(['maxlength' => 25]) ?>
+
+    <?= $form->field($model, 'account_password')->textInput(['maxlength' => 25]) ?>
+
+    <?= $form->field($model, 'account_type')->textInput(['maxlength' => 45]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
