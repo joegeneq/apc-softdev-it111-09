972cd9b5c6860b15d2ac03a2c6b299e39c012353
Martin Ayson <iammartinayson@gmail.com>
2015-04-03 14:45:21 +0800

    Modified main.php for the nav bar.

--- application/fulbright_aps/frontend/views/layouts/main.php
+++ application/fulbright_aps/frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Welcome to Fulbright Science School',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
