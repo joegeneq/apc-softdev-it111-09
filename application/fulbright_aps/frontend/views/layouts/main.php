3128eca6b7179a9c344562f001c7fb454800e761
Martin Ayson <iammartinayson@gmail.com>
2015-04-03 15:05:44 +0800

    Edited nav bar, tried to put dropdown list.

--- application/fulbright_aps/frontend/views/layouts/main.php
+++ application/fulbright_aps/frontend/views/layouts/main.php
@@ -26,20 +26,20 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'Welcome to Fulbright Science School',
+                'brandLabel' => '',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
             ]);
             $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
+                ['label' => 'HOME', 'url' => ['/site/index']],
+                ['label' => 'ABOUT', 'url' => ['/site/about']],
+                ['label' => 'CONTACT', 'url' => ['/site/contact']],
             ];
             if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+                $menuItems[] = ['label' => 'SIGNUP', 'url' => ['/site/signup']];
+                $menuItems[] = ['label' => 'LOGIN', 'url' => ['/site/login']];
             } else {
                 $menuItems[] = [
                     'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
@@ -65,8 +65,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; Fulbright Science School <?= date('Y') ?></p>
         </div>
     </footer>
 
