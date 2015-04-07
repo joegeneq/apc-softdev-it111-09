2d47d6d7ef7bdf1ff220887b282e5b25eac91708
Martin Ayson <iammartinayson@gmail.com>
2015-04-07 22:32:48 +0800

    Tested bugs when logging in for Admin User

--- application/fulbright_aps/backend/views/site/index.php
+++ application/fulbright_aps/backend/views/site/index.php
@@ -19,12 +19,12 @@ $this->title = 'Fulbright Science School/Backend Admin';
             <div class="col-lg-4">
                 
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">&raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Grading System &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">&raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">CRUD &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 
