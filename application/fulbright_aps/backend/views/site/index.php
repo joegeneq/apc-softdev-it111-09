76e42946ffa079ba0d42f6edc187f9e14252db4d
Martin Ayson <iammartinayson@gmail.com>
2015-04-11 21:27:35 +0800

    Added comments for reference in the backend/views/site/index.php

--- application/fulbright_aps/backend/views/site/index.php
+++ application/fulbright_aps/backend/views/site/index.php
@@ -15,8 +15,20 @@ $this->title = 'Fulbright Science School/Backend Admin';
         
     </div>
 
-    <div class="body-content">
+    <div class="body-content" align="center">
 
      <?= Html::a('Add User', ['site/signup'], ['class'=>'btn btn-success']) ?>
+
+
+     <!-- Section for Uploading and Editing of Gallery -->
+     <?= Html::a('Edit Pictures for Gallery', ['site/'], ['class'=>'btn btn-success']) ?>
+
+
+
+     <!-- Section for Editing and Adding of Comments -->
+     <?= Html::a('Edit Comments for Testimonials', ['site/'], ['class'=>'btn btn-success']) ?>
+
+
+     
     </div>
 </div>
