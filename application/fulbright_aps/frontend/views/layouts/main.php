d7cf47ddc0b4d40a41c691cac077d7779928abc6
Jhun Michael Tabudlong <tabudlongj@gmail.com>
2015-04-17 14:52:52 -0700

    Other method of Search Engine Optimizer.

--- application/fulbright_aps/frontend/views/layouts/main.php
+++ application/fulbright_aps/frontend/views/layouts/main.php
@@ -14,6 +14,7 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+<title>Fulbright Science School Assistance Program</title>
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- application/fulbright_aps/frontend/views/site/about.php
+++ application/fulbright_aps/frontend/views/site/about.php
@@ -1,6 +1,7 @@
 
 
 <htmL>
+<title>Fulbright Science School Assistance Program</title>
 <head>
 	 <meta charset=utf-8>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
