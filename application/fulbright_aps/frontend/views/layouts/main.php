42451bfca91c19db4ee595f3aadaeaa362c8d7a6
Jhun Michael Tabudlong <tabudlongj@gmail.com>
2015-04-17 13:44:31 -0700

    Added Main file

---
+++ application/fulbright_aps/frontend/views/layouts/main.php
@@ -0,0 +1,82 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use frontend\widgets\Alert;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+    <?php $this->beginBody() ?>
+    <div class="wrap">
+    
+        <?php
+            NavBar::begin([
+                'brandLabel' => '',
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                    'class' => 'navbar-inverse navbar-fixed-top',
+                ],
+            ]);
+            $menuItems = [
+                ['label' => 'HOME', 'url' => ['/site/index']],
+<<<<<<< HEAD
+                ['label' => 'ABOUT US', 'url' => ['/site/about']],
+=======
+                ['label' => 'ABOUT', 'url' => ['/site/about']],
+            
+>>>>>>> 395e101fff8cfdc794aed3aa884bd551c09719d3
+                ['label' => 'CONTACT', 'url' => ['/site/contact']],
+                ['label' => 'FACILITIES', 'url' => ['/site/facilities']],
+            ];
+            if (Yii::$app->user->isGuest) {
+                
+                $menuItems[] = ['label' => 'LOGIN', 'url' => ['/site/login']];
+            } else {
+                $menuItems[] = [
+                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                    'url' => ['/site/logout'],
+                    'linkOptions' => ['data-method' => 'post']
+                ];
+            }
+            echo Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-right'],
+                'items' => $menuItems,
+            ]);
+            NavBar::end();
+        ?>
+
+        <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+        <p class="pull-left">&copy; Fulbright Science School <?= date('Y') ?></p>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
