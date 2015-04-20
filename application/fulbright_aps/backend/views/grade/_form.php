d47a4f20606f4ebdc33e309a16ffb612cfe93148
Martin Ayson <iammartinayson@gmail.com>
2015-04-21 06:16:38 +0800

    Modified the form for inputting of grades.

--- application/fulbright_aps/backend/views/grade/_form.php
+++ application/fulbright_aps/backend/views/grade/_form.php
@@ -3,6 +3,9 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use yii\helpers\ArrayHelper;
+use common\models\User;
+
 /* @var $this yii\web\View */
 /* @var $model backend\models\Grade */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,23 +15,17 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
+    <?= $form->field($model, 'user_id')->dropDownList(
+                    ArrayHelper::map(User::find()->where(['type'=>'Student'])->all(),'id','full_name'),
+                    ['prompt'=>"Select Name"]
+                ) ?>
 
-    <?= $form->field($model, 'first_grading')->textInput() ?>
+    <?= $form->field($model, 'grade_subject')->dropDownList([ 'Computer' => 'Computer', 'English' => 'English', 'Filipino' => 'Filipino', 'MAPEH' => 'MAPEH', 'Math' => 'Math', 'Science' => 'Science', ], ['prompt' => 'Select Subject']) ?>
 
-    <?= $form->field($model, 'second_grading')->textInput() ?>
+    <?= $form->field($model, 'grade_quarter_number')->dropDownList([ '1st Quarter' => '1st Quarter', '2nd Quarter' => '2nd Quarter', '3rd Quarter' => '3rd Quarter', '4th Quarter' => '4th Quarter' ], ['prompt'=>"Select Quarter"]
+                ) ?>
 
-    <?= $form->field($model, 'third_grading')->textInput() ?>
-
-    <?= $form->field($model, 'fourth_grading')->textInput() ?>
-
-    <?= $form->field($model, 'grade_remarks')->textInput(['maxlength' => 45]) ?>
-
-    <?= $form->field($model, 'grade_date_submitted')->textInput() ?>
-
-    <?= $form->field($model, 'student_id')->textInput() ?>
-
-    <?= $form->field($model, 'teacher_id')->textInput() ?>
+    <?= $form->field($model, 'grade_remarks')->textInput(['maxlength' => 10]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
