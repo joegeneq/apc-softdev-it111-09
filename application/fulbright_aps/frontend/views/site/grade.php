a4b94fa8d1ce1886e289c1507c8b3f38804c89bb
Jhun Michael Tabudlong <tabudlongj@gmail.com>
2015-04-20 13:33:49 -0700

    Added grade page

---
+++ application/fulbright_aps/frontend/views/site/grade.php
@@ -0,0 +1,62 @@
+<?php
+use backend\models\GradeSearch;
+
+?>
+
+<html>
+
+
+<head>
+</head>
+
+
+
+<body>
+	
+ <div class="section secondary-section " id="portfolio">
+            <div class="triangle"></div>
+            <div class="container">
+                <div class=" title">
+                    <h1>Online Grades Views</h1>
+                    <p></p>
+                </div>
+
+
+                <!-- Details for School Gallery -->
+                <?php
+                    $searchModel = new GradeSearch();
+                    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+                    foreach ($dataProvider->models as $model) {
+                    
+                    echo '<div id="single-project">
+                    <div id="slidingDiv'.$model->id.'" class="toggleDiv row-fluid single-project">
+                        <div class="span6">
+                       
+                        </div>
+                        <div class="span6">
+                            <div class="project-description">
+                                <div class="project-title clearfix">
+                                    <h3>'.$model->first_grading.'</h3>
+                                    <span class="show_hide close">
+                                        <i class="icon-cancel"></i>
+                                    </span>
+                                </div>
+                                <div class="project-info">
+                                    <div>
+                                        <span></span>'.$model->first_grading.'</div>
+                                        
+                                </div>
+                                <p>'.$model->first_grading.'</p>
+                            </div>
+                        </div>
+                    </div>';
+                }?> 
+
+
+                </div>
+            </div>
+        </div>
+
+</body>
+</html>
